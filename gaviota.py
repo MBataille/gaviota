a1c1c26eb52edd5ceb21a14a4a73e37c88dab92c
sesepulveda17 <38366209+sesepulveda17@users.noreply.github.com>
2018-04-15 13:58:08 -0300

    Update gaviota.py

--- gaviota.py
+++ gaviota.py
@@ -7,26 +7,29 @@ t = np.linspace(0,10,1001)
 dt = t[1]
 v_z = 0.5
 
-scene.title = "<b>A bird is flying</b>"
+scene.title = "<b>A bird is flying</b></n>"
 scene.width = 640
 scene.height = 600
 scene.forward = vector(0,-.3,-1)
 
+#Pajaro creado
 gaviota = sphere(pos=vector(r,0,0), radius=r/10, color=color.cyan,
 		make_trail=True, interval=1)
-x_axis = arrow(pos=vector(0,0,0), axis=vector(1,0,0), color=color.red)
-y_axis = arrow(pos=vector(0,0,0), axis=vector(0,1,0), color=color.blue)
-z_axis = arrow(pos=vector(0,0,0), axis=vector(0,0,1), color=color.green)
+#Vectores unitarios
+x_axis = arrow(pos=vector(0,0,0), axis=vector(1,0,0), color=color.red, shaftwidth=0.05)
+y_axis = arrow(pos=vector(0,0,0), axis=vector(0,1,0), color=color.blue, shaftwidth=0.05)
+z_axis = arrow(pos=vector(0,0,0), axis=vector(0,0,1), color=color.green, shaftwidth=0.05)
 
+#Vectores de trayectoria
 rarr = arrow(pos=gaviota.pos, axis=vector(1,0,0), color=color.magenta)
 tarr = arrow(pos=gaviota.pos, axis=vector(0,1,0), color=color.white)
 
-scene.autoscale = False
+scene.autoscale = True
 
 #aqui escribimos la descripcion del video
 scene.append_to_caption("""Una gaviota puede planear, a pesar de la gravedad,
-	gracias a una fuerza de origen aerodinamico, conocida como fuerza de sustentacion, L.
-	El movimiento es descrito en la representacion visual""")
+gracias a una fuerza de origen aerodinamico, conocida como fuerza de sustentacion, L.
+El movimiento es descrito en esta representacion visual""")
 
 
 while True:

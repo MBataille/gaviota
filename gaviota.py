dfecb62de4efc1c92b3fbe3c04ebc3d34cd841b6
Martin <martinbataille@gmail.com>
2018-04-16 23:17:43 -0300

    Agregada fuerza de sustentacion + Cambio de nombres de ijk

--- gaviota.py
+++ gaviota.py
@@ -5,6 +5,8 @@ r = 2
 omega = 5
 t = np.linspace(0,10,1001)
 v_z = 0.5
+beta = np.pi/4
+
 
 scene.title = "<b>A bird is flying</b></n>"
 scene.width = 640
@@ -15,14 +17,17 @@ scene.forward = vector(0,-.3,-1)
 gaviota = sphere(pos=vector(r,0,0), radius=r/10, color=color.cyan,
 		make_trail=True, interval=1)
 # Vectores unitarios, i, j , k
-x_axis = arrow(pos=vector(0,0,0), axis=vector(1,0,0), color=color.red, shaftwidth=0.05)
-y_axis = arrow(pos=vector(0,0,0), axis=vector(0,1,0), color=color.blue, shaftwidth=0.05)
-z_axis = arrow(pos=vector(0,0,0), axis=vector(0,0,1), color=color.green, shaftwidth=0.05)
+x_i = arrow(pos=vector(0,0,0), axis=vector(1,0,0), color=color.red, shaftwidth=0.05)
+y_j = arrow(pos=vector(0,0,0), axis=vector(0,1,0), color=color.blue, shaftwidth=0.05)
+z_k = arrow(pos=vector(0,0,0), axis=vector(0,0,1), color=color.green, shaftwidth=0.05)
 
 # Vectores de trayectoria, r y theta tambien unitarios
 rarr = arrow(pos=gaviota.pos, axis=vector(1,0,0), color=color.magenta)
 tarr = arrow(pos=gaviota.pos, axis=vector(0,1,0), color=color.white)
 
+Larr = arrow(pos=gaviota.pos, axis=np.sin(beta)*rarr.axis
+								+np.cos(beta)*z_k.axis,
+			color=color.orange)
 scene.autoscale = True
 
 # aqui escribimos la descripcion del video
@@ -35,13 +40,14 @@ while True:
 	for this_t in t:  # Actualiza la posicion de la gaviota
 		# y la direccion de los vectores unitarios r, theta
 		rate(60)  # Espera 1/60 s para que no se vea tan rapido
- 		theta = omega*this_t
+		theta = omega*this_t
 
  		# actualiza la direccion de r y theta
 		rarr.axis = vector(np.cos(theta),np.sin(theta),0)
 		tarr.axis = vector(-np.sin(theta), np.cos(theta), 0)
+		Larr.axis = -np.sin(beta)*rarr.axis + np.cos(beta)*z_k.axis		
 
 		# actualiza la direccion de r, theta y la gaviota
-		rarr.pos = tarr.pos = gaviota.pos = vector(r*np.cos(theta), r*np.sin(theta),
+		Larr.pos = rarr.pos = tarr.pos = gaviota.pos = vector(r*np.cos(theta), r*np.sin(theta),
 			v_z*this_t)
 	res = input('Press any key to reset')

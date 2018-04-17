481b1450af4a2f1c21d427f2614a3c61e567241a
Sebasitan Sepulveda <38366209+sesepulveda17@users.noreply.github.com>
2018-04-17 02:23:53 -0300

    Nueva imagen con mas dibujos

--- gaviota.py
+++ gaviota.py
@@ -1,13 +1,16 @@
 from vpython import *
 import numpy as np
 
+#Parametros base
 r = 2
 omega = 5
 t = np.linspace(0,10,1001)
 dt = t[1]
 v_z = 0.5
+beta = np.pi/4
 
 scene.title = "<b>A bird is flying</b></n>"
+#Ajustamos tamaño de pantalla
 scene.width = 640
 scene.height = 600
 scene.forward = vector(0,-.3,-1)
@@ -16,13 +19,25 @@ scene.forward = vector(0,-.3,-1)
 gaviota = sphere(pos=vector(r,0,0), radius=r/10, color=color.cyan,
 		make_trail=True, interval=1)
 #Vectores unitarios
-x_axis = arrow(pos=vector(0,0,0), axis=vector(1,0,0), color=color.red, shaftwidth=0.05)
-y_axis = arrow(pos=vector(0,0,0), axis=vector(0,1,0), color=color.blue, shaftwidth=0.05)
-z_axis = arrow(pos=vector(0,0,0), axis=vector(0,0,1), color=color.green, shaftwidth=0.05)
+x_i = arrow(pos=vector(0,0,0), axis=vector(1,0,0), color=color.red, shaftwidth=0.05)
+y_j = arrow(pos=vector(0,0,0), axis=vector(0,1,0), color=color.blue, shaftwidth=0.05)
+z_k = arrow(pos=vector(0,0,0), axis=vector(0,0,1), color=color.green, shaftwidth=0.05)
 
-#Vectores de trayectoria
-rarr = arrow(pos=gaviota.pos, axis=vector(1,0,0), color=color.magenta)
-tarr = arrow(pos=gaviota.pos, axis=vector(0,1,0), color=color.white)
+#Vectores de trayectoria con sus respectivas letras
+rarr = arrow(pos=gaviota.pos, axis=vector(1,0,0), color=color.magenta, shaftwidth=0.05)
+txt_rarr = text(text='n', pos=rarr.pos+rarr.axis, axis=rarr.axis, align='center', height=0.4,
+          color=color.magenta, billboard=True, emissive=True)
+
+tarr = arrow(pos=gaviota.pos, axis=vector(0,1,0), color=color.white, shaftwidth=0.05)
+txt_tarr = text(text='t', pos=tarr.pos+tarr.axis, axis=tarr.axis, align='center', height=0.4,
+          color=color.white, billboard=True, emissive=True)
+
+Larr = arrow(pos=gaviota.pos, axis=np.sin(beta)*rarr.axis
+								+np.cos(beta)*z_k.axis, shaftwidth=0.05,
+			color=color.orange)
+txt_Larr = text(text='L', pos=Larr.pos+Larr.axis, axis=Larr.axis, align='center', height=0.4,
+          color=color.orange, billboard=True, emissive=True)
+
 
 scene.autoscale = True
 
@@ -34,10 +49,21 @@ El movimiento es descrito en esta representacion visual""")
 
 while True:
 	for this_t in t:
+		'''Actualiza la posicion de la gaviota
+		y la direccion de los vectores unitarios r, theta'''
 		rate(60)
-		theta = omega*this_t
+		theta = omega*this_t	# Espera 1/60 s para que no se vea tan rapido
+		# actualiza la direccion de r y theta
 		rarr.axis = vector(np.cos(theta),np.sin(theta),0)
 		tarr.axis = vector(-np.sin(theta), np.cos(theta), 0)
-		rarr.pos = tarr.pos = gaviota.pos = vector(r*np.cos(theta), r*np.sin(theta),
+		Larr.axis = -np.sin(beta)*rarr.axis + np.cos(beta)*z_k.axis	
+
+		# actualiza la direccion de r, theta y la gaviota
+		Larr.pos = rarr.pos = tarr.pos = gaviota.pos = vector(r*np.cos(theta), r*np.sin(theta),
 			v_z*this_t)
+			
+		#generamos las letras
+		txt_rarr.pos = rarr.pos+rarr.axis
+		txt_tarr.pos = tarr.pos+tarr.axis
+		txt_Larr.pos = Larr.pos+Larr.axis
 	res = input('Press any key to reset')
